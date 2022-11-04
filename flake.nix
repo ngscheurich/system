96a2ae78123e6b87c36a03c3c1eb8a9ea041c813
N. G. Scheurich <nick@scheurich.me>
2022-11-04 16:38:32 -0500

    Add Nix devShells

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "Incantations; or, A Set of Declarative System Configurations";
 
+  nixConfig.bash-prompt = "\\033[32m\[nix-develop\:$PWD]$\\033[0m ";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
@@ -10,11 +12,23 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    flake-utils.url = "github:numtide/flake-utils";
+
     neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = { self, nixpkgs, darwin, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, darwin, home-manager, flake-utils, ... }@inputs:
     let
+      inherit (flake-utils.lib) eachSystemMap;
+
+      defaultSystems =
+        [
+          "aarch64-linux"
+          "aarch64-darwin"
+          "x86_64-darwin"
+          "x86_64-linux"
+        ];
+
       overlays = [ inputs.neovim-nightly.overlay ];
 
       mkDarwinSystem =
@@ -58,5 +72,19 @@
           extraModules = [ ./hosts/twoflower ];
         };
       };
+
+      devShells = eachSystemMap defaultSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              rustc
+              cargo
+              rust-analyzer
+            ];
+          };
+        });
     };
 }
