89d65a60249a6bd2a501e20afdfc92e57f5d59ab
N. G. Scheurich <nick@scheurich.me>
2022-12-31 15:25:20 -0600

    Update telescope prompts/prefix

--- dotfiles/nvim/lua/config/plugins/telescope.lua
+++ dotfiles/nvim/lua/config/plugins/telescope.lua
@@ -1,23 +1,31 @@
+local M = { "nvim-telescope/telescope.nvim" }
+
 local function builtin(source)
   require("telescope.builtin")[source]()
 end
 
-return {
-  "nvim-telescope/telescope.nvim",
+M.dependencies = { "nvim-lua/plenary.nvim" }
 
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
+function M.config()
+  require("telescope").setup({
+    defaults = {
+      prompt_prefix = " ❯ ",
+      selection_caret = " ❯ ",
+      entry_prefix = "   ",
+    },
+  })
+end
 
-  keys = {
-    { "<Leader>/", function() builtin("current_buffer_fuzzy_find") end, desc = "Search" },
-    { "<Leader><Space>", function() builtin("buffers") end, desc = "Buffers" },
-    { "<Leader>fa", function() builtin("autocommands") end, desc = "Autocommands" },
-    { "<Leader>fc", function() builtin("commands") end, desc = "Commands" },
-    { "<Leader>ff", function() builtin("find_files") end, desc = "Files" },
-    { "<Leader>fg", function() builtin("live_grep") end, desc = "Grep" },
-    { "<Leader>fh", function() builtin("help_tags") end, desc = "Help" },
-    { "<Leader>fl", function() builtin("loclist") end, desc = "Location list" },
-    { "<Leader>fq", function() builtin("quickfix") end, desc = "Quickfix list" },
-  }
+M.keys = {
+  { "<Leader>/", function() builtin("current_buffer_fuzzy_find") end, desc = "Search" },
+  { "<Leader><Space>", function() builtin("buffers") end, desc = "Buffers" },
+  { "<Leader>fa", function() builtin("autocommands") end, desc = "Autocommands" },
+  { "<Leader>fc", function() builtin("commands") end, desc = "Commands" },
+  { "<Leader>ff", function() builtin("find_files") end, desc = "Files" },
+  { "<Leader>fg", function() builtin("live_grep") end, desc = "Grep" },
+  { "<Leader>fh", function() builtin("help_tags") end, desc = "Help" },
+  { "<Leader>fl", function() builtin("loclist") end, desc = "Location list" },
+  { "<Leader>fq", function() builtin("quickfix") end, desc = "Quickfix list" },
 }
+
+return M
