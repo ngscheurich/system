6078cb325377572953ac3755e316a4d4288990a4
N. G. Scheurich <nick@scheurich.me>
2022-12-31 14:35:20 -0600

    Add icons to lualine

--- dotfiles/nvim/lua/config/plugins/lualine.lua
+++ dotfiles/nvim/lua/config/plugins/lualine.lua
@@ -1,11 +1,23 @@
-return {
-  "nvim-lualine/lualine.nvim",
+local M = { "nvim-lualine/lualine.nvim" }
 
-  config = {
+function M.config()
+  local icons = require("nvim-nonicons")
+
+  require("lualine").setup({
     options = {
-      icons_enabled = false,
-      component_separators = "│",
+      component_separators = "",
       section_separators = "",
     },
-  },
-}
+
+    sections = {
+      lualine_b = {
+        {
+          "branch",
+          icon = icons.get("git-branch"),
+        },
+      },
+    },
+  })
+end
+
+return M
