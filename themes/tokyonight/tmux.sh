8958e3d358a3e20c641a146a3bf0242c2ad1c301
N. G. Scheurich <nick@scheurich.me>
2023-01-24 18:55:37 -0600

    Add Tokyo Night theme

---
+++ themes/tokyonight/tmux.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export TMUX_COLOR_MAIN=#16161e
+export TMUX_COLOR_ACTIVE=#1a1b26
+export TMUX_COLOR_SEG_1_FG=colour15
+export TMUX_COLOR_SEG_1_BG=#292e42
+export TMUX_COLOR_SEG_2_FG=colour4
+export TMUX_COLOR_SEG_2_BG=#33467c
+export TMUX_COLOR_SEG_3_FG=colour0
+export TMUX_COLOR_SEG_3_BG=#7aa2f7
+
+"$HOME/scripts/tmux-statusline.sh"
