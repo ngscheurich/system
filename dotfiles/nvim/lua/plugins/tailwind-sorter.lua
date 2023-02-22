a82e006f276303dd4259c4b7d4e82f405f02ce7e
N. G. Scheurich <nick@scheurich.me>
2023-02-21 20:20:43 -0600

    Add tailwind-sorter

---
+++ dotfiles/nvim/lua/plugins/tailwind-sorter.lua
@@ -0,0 +1,15 @@
+return {
+  "laytan/tailwind-sorter.nvim",
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-lua/plenary.nvim"
+  },
+
+  build = "cd formatter && npm i && npm run build",
+
+  config = {
+    on_save_enabled = true,
+    on_save_pattern = { "*.html", "*.heex", "*.ex" },
+  },
+}
