74fce99fe20965bfc24e655c8d3849aebcfbbcc1
N. G. Scheurich <nick@scheurich.me>
2023-03-03 00:13:43 -0600

    Set more colors

--- scripts/tmux-statusline.sh
+++ scripts/tmux-statusline.sh
@@ -4,9 +4,10 @@ set -e
 
 SCRIPTS=$HOME/scripts
 
+tmux set -g status-left "#[fg=$TMUX_COLOR_LEFT_FG,bg=$TMUX_COLOR_LEFT_BG] #S "
 tmux set -g status-style "fg=colour0,bg=$TMUX_COLOR_MAIN"
 tmux set -g window-status-style "fg=colour7,bg=$TMUX_COLOR_MAIN"
-tmux set -g window-status-current-format "#[fg=colour15,bg=$TMUX_COLOR_ACTIVE]$WINDOW"
+tmux set -g window-status-current-format "#[fg=$TMUX_COLOR_ACTIVE_FG,bg=$TMUX_COLOR_ACTIVE_BG]$WINDOW"
 tmux set -g status-right "#[fg=$TMUX_COLOR_SEG_1_FG,bg=$TMUX_COLOR_SEG_1_BG] \
 #(whoami)@#($SCRIPTS/tmux-hostname.sh) \
 #[fg=$TMUX_COLOR_SEG_2_FG,bg=$TMUX_COLOR_SEG_2_BG] #{?window_zoomed_flag, ,}#{?client_prefix, , } \
