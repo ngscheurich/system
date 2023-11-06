f87912c966a6ba56a41754aca1260cd33bb87637
N. G. Scheurich <nick@scheurich.me>
2023-11-06 11:52:47 -0600

    [nvim] Update elixir-ls cmd location

--- dotfiles/nvim/lua/ngs/plugins/lang/elixir.lua
+++ dotfiles/nvim/lua/ngs/plugins/lang/elixir.lua
@@ -3,6 +3,6 @@ local util = require("ngs.util")
 return {
   util.treesitter_ensure("elixir"),
   util.lspconfig_setup("elixirls", {
-    cmd = { "/Users/nick/Projects/elixir-ls/release/language_server.sh" },
+    cmd = { vim.env.HOME .. "/.local/share/nvim/mason/bin/elixir-ls" },
   }),
 }
