37d5cb2627977c4dd4d75c75443e159a08d97590
N. G. Scheurich <nick@scheurich.me>
2023-11-06 11:55:32 -0600

    [nix] Simplify ridcully config

--- hosts/ridcully/default.nix
+++ hosts/ridcully/default.nix
@@ -1,7 +1,31 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  imports = [
-    ./home.nix
-  ];
+  users = {
+    knownUsers = [ "nscheurich" ];
+
+    users.nick = {
+      home = "/Users/nscheurich";
+      description = "N. G. Scheurich";
+      shell = pkgs.fish;
+      uid = 501;
+    };
+  };
+
+  home-manager.users = {
+    nscheurich = {
+      home.stateVersion = "22.11";
+
+      imports = [
+        ../../modules/home/code.nix
+        ../../modules/home/crypt.nix
+        ../../modules/home/devel.nix
+        ../../modules/home/devops.nix
+        ../../modules/home/git.nix
+        ../../modules/home/prose.nix
+        ../../modules/home/shell.nix
+        ../../modules/home/vim.nix
+      ];
+    };
+  };
 }
