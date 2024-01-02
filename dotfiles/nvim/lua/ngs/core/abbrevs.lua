bd3114aacc47e70a926cb75f1c0cef214b5773db
N. G. Scheurich <nick@scheurich.me>
2024-01-02 12:15:01 -0600

    [nvim] Add some GridPoint abbrevs

--- dotfiles/nvim/lua/ngs/core/abbrevs.lua
+++ dotfiles/nvim/lua/ngs/core/abbrevs.lua
@@ -5,6 +5,9 @@
 -- Insert mode
 vim.cmd.iabbrev("Gridpoint", "GridPoint")
 vim.cmd.iabbrev("GridpointWeb", "GridPointWeb")
+vim.cmd.iabbrev("dp", "duct_probe")
+vim.cmd.iabbrev("zsm", "zone_sensor_module")
+vim.cmd.iabbrev("rsm", "remote_sensor_module")
 
 -- Command-line mode
 vim.cmd.cabbrev("Qa", "qa")
