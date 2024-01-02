71637fa883047de2ccb289cc604843d3f5168295
N. G. Scheurich <nick@scheurich.me>
2024-01-02 12:16:08 -0600

    [nvim] Add nvim-treesitter-textobjects

--- dotfiles/nvim/lua/ngs/plugins/treesitter.lua
+++ dotfiles/nvim/lua/ngs/plugins/treesitter.lua
@@ -1,10 +1,29 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	opts = {
-		highlight = { enable = true },
-		indent = { enable = true },
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependecies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          select = {
+            enable = true,
+            keymaps = {
+              ["ib"] = "@block.inner",
+              ["ab"] = "@block.outer",
+            },
+          },
+        },
+      })
+    end,
+  },
 }
