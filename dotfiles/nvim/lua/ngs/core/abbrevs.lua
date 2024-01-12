939a29de2374b1e08a2a2697c65f51332f5d9bdd
N. G. Scheurich <nick@scheurich.me>
2024-01-12 14:06:23 -0600

    [nvim] Update abbreviations

--- dotfiles/nvim/lua/ngs/core/abbrevs.lua
+++ dotfiles/nvim/lua/ngs/core/abbrevs.lua
@@ -5,10 +5,9 @@
 -- Insert mode
 vim.cmd.iabbrev("Gridpoint", "GridPoint")
 vim.cmd.iabbrev("GridpointWeb", "GridPointWeb")
-vim.cmd.iabbrev("dp", "duct_probe")
-vim.cmd.iabbrev("zsm", "zone_sensor_module")
-vim.cmd.iabbrev("rsm", "remote_sensor_module")
 
 -- Command-line mode
 vim.cmd.cabbrev("Qa", "qa")
 vim.cmd.cabbrev("Xa", "xa")
+vim.cmd.cabbrev("Qa!", "qa")
+vim.cmd.cabbrev("Xa!", "xa")
