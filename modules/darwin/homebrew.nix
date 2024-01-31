17351d00b6379f4e0a59e237b475c222294d5142
N. G. Scheurich <nick@scheurich.me>
2024-01-31 11:39:03 -0600

    [nix] Switch to emacs-mac

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -13,58 +13,11 @@
     taps = [
       "homebrew/cask-versions"
       "homebrew/services"
-      "d12frosted/emacs-plus"
+      "railwaycat/emacsmacport"
     ];
 
     brews = [
-      {
-        name = "emacs-plus@29";
-        args = [ "with-xwidgets" "with-savchenkovaleriy-big-sur-icon" ];
-      }
       "llvm"
-
-      # Emacs dependencies
-      "gmp"
-      "libunistring"
-      "gettext"
-      "giflib"
-      "libidn2"
-      "libtasn1"
-      "nettle"
-      "p11-kit"
-      "openssl@1.1"
-      "libevent"
-      "libnghttp2"
-      "unbound"
-      "gnutls"
-      "libpng"
-      "freetype"
-      "fontconfig"
-      "pcre2"
-      "glib"
-      "xorgproto"
-      "libxau"
-      "libxdmcp"
-      "libxcb"
-      "libx11"
-      "libxext"
-      "libxrender"
-      "lzo"
-      "pixman"
-      "cairo"
-      "jpeg-turbo"
-      "libtiff"
-      "gdk-pixbuf"
-      "fribidi"
-      "graphite2"
-      "icu4c"
-      "harfbuzz"
-      "pango"
-      "librsvg"
-      "little-cms2"
-      "jansson"
-      "tree-sitter"
-      "webp"
     ];
 
     casks = [
@@ -79,6 +32,7 @@
       "discord"
       "docker"
       "dropbox"
+      "emacs-mac"
       "fantastical"
       "figma"
       "firefox"
