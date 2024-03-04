1300028d5ce94902c8b55f6f7dfcd1eea0b9672a
N. G. Scheurich <nick@scheurich.me>
2024-03-04 04:38:05 -0600

    [nvim] Set diagnostic info sign correctly

--- dotfiles/nvim/lua/ngs/core/signs.lua
+++ dotfiles/nvim/lua/ngs/core/signs.lua
@@ -3,13 +3,10 @@
 ------------------------------------------------------------
 
 local function set_sign_text(severity, text)
-  vim.fn.sign_define(
-    "DiagnosticSign" .. severity,
-    { text = text, texthl = "Diagnostic" .. severity }
-  )
+  vim.fn.sign_define("DiagnosticSign" .. severity, { text = text, texthl = "Diagnostic" .. severity })
 end
 
 set_sign_text("Error", "")
 set_sign_text("Warn", "")
-set_sign_text("Information", "")
+set_sign_text("Info", "")
 set_sign_text("Hint", "")
