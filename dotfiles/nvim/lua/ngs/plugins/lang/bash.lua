d8867a57bb02ee5e3cb6d5419e275f965cff9210
N. G. Scheurich <nick@scheurich.me>
2024-03-05 19:28:09 -0600

    [nvim] Add Bash header snippet

--- dotfiles/nvim/lua/ngs/plugins/lang/bash.lua
+++ dotfiles/nvim/lua/ngs/plugins/lang/bash.lua
@@ -1,8 +1,17 @@
+local ls = require("luasnip")
 local util = require("ngs.util")
+local s, t = ls.snippet, ls.text_node
 
 return {
   util.treesitter_ensure("bash"),
   util.lspconfig_setup("bashls"),
   util.formatter_setup("sh", { "shfmt" }),
   util.linter_setup("sh", { "shellcheck" }),
+  util.luasnip_add("sh", {
+    s("bash-header", { t({
+      "#!/usr/bin/env bash",
+      "",
+      "set -euo pipefail",
+    }) }),
+  }),
 }
