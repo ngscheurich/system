cba43bc3d68fa81d0cf07d4d203a8d12a80394d1
N. G. Scheurich <nick@scheurich.me>
2024-03-18 13:13:19 -0500

    [nvim] Add SwitchTheme user command

--- dotfiles/nvim/lua/ngs/core/commands.lua
+++ dotfiles/nvim/lua/ngs/core/commands.lua
@@ -13,3 +13,23 @@ autocmd({ "TextYankPost" }, {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_user_command("SwitchTheme", function()
+  local theme_paths = vim.split(vim.fn.globpath("/etc/system/themes", "*"), "\n")
+
+  local themes = vim.tbl_map(function(value)
+    return vim.fn.fnamemodify(value, ":t")
+  end, theme_paths)
+
+  local colorschemes = {
+    ["gruvbox-dark"] = "catppuccin-mocha",
+    ["gruvbox-light"] = "catppuccin-latte",
+  }
+
+  vim.ui.select(themes, {
+    prompt = "Select theme:",
+  }, function(choice)
+    io.popen(string.format("switch-theme %s", choice))
+    vim.cmd.colorscheme(colorschemes[choice] or choice)
+  end)
+end, {})
