849e06554377452cb5dba8dc608818a442f5a836
N. G. Scheurich <nick@scheurich.me>
2024-03-20 01:37:00 -0500

    [nvim] Use elixir-ls for Elixir

--- dotfiles/nvim/lua/ngs/plugins/lang/elixir.lua
+++ dotfiles/nvim/lua/ngs/plugins/lang/elixir.lua
@@ -1,13 +1,16 @@
-local util = require("ngs.util")
 local ls = require("luasnip")
+local util = require("ngs.util")
 local s, t, i = ls.snippet, ls.text_node, ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 
 return {
   util.treesitter_ensure("elixir"),
-  util.lspconfig_setup("lexical", {
-    cmd = { vim.fn.expand("~") .. "/Projects/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
+  -- util.lspconfig_setup("lexical", {
+  --   cmd = { vim.fn.expand("~") .. "/Projects/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
+  -- }),
+  util.lspconfig_setup("elixirls", {
+    cmd = { vim.fn.stdpath("data") .. "/mason/bin/elixir-ls" },
   }),
   util.luasnip_add("elixir", {
     s("pry", { t({
