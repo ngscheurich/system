e76316476521ce2bed531aa68971625af136f1b2
N. G. Scheurich <nick@scheurich.me>
2024-03-24 23:34:56 -0500

    [nvim] Update Copilot keys

--- dotfiles/nvim/lua/plugins/ai.lua
+++ dotfiles/nvim/lua/plugins/ai.lua
@@ -13,6 +13,7 @@ return {
     "github/copilot.vim",
     config = function()
       vim.cmd("Copilot disable")
+
       vim.keymap.set("n", "<Leader>ai", function()
         local subcmd = "enable"
         if vim.fn["copilot#Enabled"]() == 1 then
@@ -21,8 +22,12 @@ return {
         vim.cmd("Copilot " .. subcmd)
         vim.print("Copilot " .. subcmd .. "d")
       end, { desc = "Toggle Copilot" })
+
       vim.keymap.set("n", "<Leader>ap", "<Cmd>Copilot panel<CR>", { desc = "Copilot panel" })
       vim.keymap.set("n", "<Leader>as", "<Cmd>Copilot status<CR>", { desc = "Copilot status" })
+      vim.keymap.set("i", "<C-O>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
+
+      vim.g.copilot_no_tab_map = true
     end,
   },
 }
