2865fa958da7565dfe6bba9a0228f9823fc90bb2
N. G. Scheurich <nick@scheurich.me>
2024-05-06 09:09:08 -0500

    [themes] Add tomorrow-night

---
+++ themes/tomorrow-night/tmux.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export TMUX_COLOR_MAIN=#363a41
+
+export TMUX_COLOR_LEFT_FG=colour0
+export TMUX_COLOR_LEFT_BG=colour4
+
+export TMUX_COLOR_ACTIVE_FG=colour7
+export TMUX_COLOR_ACTIVE_BG=colour0
+
+export TMUX_COLOR_SEG_1_FG=colour7
+export TMUX_COLOR_SEG_1_BG=#363a41
+
+export TMUX_COLOR_SEG_2_FG=colour4
+export TMUX_COLOR_SEG_2_BG=#282a2e
+
+export TMUX_COLOR_SEG_3_FG=colour0
+export TMUX_COLOR_SEG_3_BG=colour7
+
+tmux-statusline
