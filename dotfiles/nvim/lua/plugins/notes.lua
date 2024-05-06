d5761b531b3dfde4eacc2da31f73c0d4ae877c91
N. G. Scheurich <nick@scheurich.me>
2024-05-06 09:34:37 -0500

    [nvim] Update obsidian.nvim config

--- dotfiles/nvim/lua/plugins/notes.lua
+++ dotfiles/nvim/lua/plugins/notes.lua
@@ -12,20 +12,16 @@ return {
   {
     "epwalsh/obsidian.nvim",
     version = "*",
-    lazy = true,
-    ft = "markdown",
     dependencies = { "nvim-lua/plenary.nvim" },
-    event = {
-      "BufReadPre " .. vim.fn.expand("~") .. "/Notes/**.md",
-      "BufNewFile " .. vim.fn.expand("~") .. "/Notes/**.md",
-    },
     opts = {
       workspaces = {
         {
           name = "Notes",
-          -- TODO: Expand path
-          path = "~/Notes",
+          path = vim.fn.expand("~") .. "/Notes",
         },
+      },
+      daily_notes = {
+        folder = vim.fn.expand("~") .. "/Notes/Daily\\ Notes",
       },
     },
   },
