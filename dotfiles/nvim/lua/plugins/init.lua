3692851038b3c42fd7ba03e8bd0e1e7c02b1f66f
N. G. Scheurich <nick@scheurich.haus>
2024-08-29 00:25:44 -0500

    [nvim] Add TailwindCSS plugins for evaluation

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -77,4 +77,23 @@ return {
       },
     },
   },
+
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = true,
+  },
+
+  {
+    "laytan/tailwind-sorter.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+    build = "cd formatter && npm ci && npm run build",
+    config = function()
+      require("tailwind-sorter").setup({
+        on_save_enabled = true, -- If `true`, automatically enables on save sorting.
+        on_save_pattern = { "*.html", "*.js", "*.jsx", "*.tsx", "*.heex", "*.ex" }, -- The file patterns to watch and sort.
+        node_path = "node",
+      })
+    end,
+  },
 }
