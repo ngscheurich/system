b4d0752dc410adef6d23865e34efb590c5815562
N. G. Scheurich <nick@scheurich.haus>
2024-08-31 14:36:07 -0500

    [scripts] Update scripts

--- scripts/reload-theme.sh
+++ scripts/reload-theme.sh
@@ -7,5 +7,6 @@ THEME_PATH="$HOME/.theme"
 kitty @ set-colors --all --configured "$THEME_PATH/kitty.conf"
 kitty @ load-config "$XDG_CONFIG_HOME/kitty/kitty.conf"
 
+# shellcheck disable=SC1091
 source "$THEME_PATH/shell.sh"
 "$THEME_PATH/tmux.sh"
