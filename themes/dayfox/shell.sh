07dafd19bedf4606c1c2eb232dc5660189143be6
N. G. Scheurich <nick@scheurich.haus>
2024-08-30 23:07:34 -0500

    [themes] Update *fox FZF colors

--- themes/dayfox/shell.sh
+++ themes/dayfox/shell.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-readonly FZF_COLOR="fg:7,bg:0,hl:8,fg+:3,bg+:0,hl+:1,gutter:0,info:6,prompt:2,pointer:4,marker:1,spinner:5"
+FZF_COLOR="fg:0,bg:#f2e9e1,hl:11,fg+:3,bg+:#f2e9e1,hl+:1,gutter:#f2e9e1,info:6,prompt:2,pointer:4,marker:1,spinner:5"
 export FZF_DEFAULT_OPTS="--color $FZF_COLOR"
