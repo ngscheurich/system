1121efc3921e97ea188d0134879d7e940a603de5
N. G. Scheurich <nick@scheurich.haus>
2024-10-08 17:06:05 -0500

    [themes] Updates

--- themes/tomorrow-night/shell.sh
+++ themes/tomorrow-night/shell.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-readonly FZF_COLOR="fg:7,bg:0,hl:8,fg+:3,bg+:0,hl+:1,gutter:0,info:6,prompt:2,pointer:4,marker:1,spinner:5"
+FZF_COLOR="fg:7,bg:0,hl:8,fg+:3,bg+:0,hl+:1,gutter:0,info:6,prompt:2,pointer:4,marker:1,spinner:5"
 export FZF_DEFAULT_OPTS="--color $FZF_COLOR"
