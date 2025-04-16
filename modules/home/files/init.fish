0aca741d878f7f38da172353de11ed383421641f
N. G. Scheurich <nick@scheurich.haus>
2025-04-15 21:26:08 -0500

    [fish] Don't set NVIM_LISTEN_ADDRESS

--- modules/home/files/init.fish
+++ modules/home/files/init.fish
@@ -8,7 +8,6 @@ set -gx EDITOR nvim
 set -gx ERL_AFLAGS '-kernel shell_history enabled'
 set -gx GPG_TTY (tty)
 set -gx KERL_CONFIGURE_OPTIONS --without-javac
-set -gx NVIM_LISTEN_ADDRESS /tmp/nvimsocket
 set -gx PLAYDATE_SDK_PATH $HOME/Developer/PlaydateSDK/
 set -gx RANGER_DEVICONS_SEPARATOR '  '
 set -gx XDG_DATA_HOME $HOME/.local/share
