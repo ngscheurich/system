cb2fabb94b1b716a4b7fbc5bb17899dc8b6098f1
N. G. Scheurich <nick@scheurich.haus>
2025-04-30 16:49:18 -0500

    [themes] Update viridian

--- themes/viridian/shell.fish
+++ themes/viridian/shell.fish
@@ -1,2 +1,2 @@
-set fzf_color "fg:7,bg:#155352,hl:8,fg+:3,bg+:#155352,hl+:1,gutter:#155352,info:6,prompt:2,pointer:4,marker:1,spinner:5"
+set fzf_color "fg:7,bg:#264648,hl:8,fg+:3,bg+:#264648,hl+:1,gutter:#264648,info:6,prompt:2,pointer:4,marker:1,spinner:5"
 set -gx FZF_DEFAULT_OPTS "--color $fzf_color"
--- themes/viridian/tmux.sh
+++ themes/viridian/tmux.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 export TMUX_COLOR_MAIN_FG=colour7
-export TMUX_COLOR_MAIN_BG=#2c6463
+export TMUX_COLOR_MAIN_BG=#3e5a5d
 
 export TMUX_COLOR_LEFT_FG=#0f3b3a
 export TMUX_COLOR_LEFT_BG=colour2
@@ -12,12 +12,12 @@ export TMUX_COLOR_ACTIVE_FG=colour7
 export TMUX_COLOR_ACTIVE_BG=#0f3b3a
 
 export TMUX_COLOR_SEG_1_FG=colour7
-export TMUX_COLOR_SEG_1_BG=#2c6463
+export TMUX_COLOR_SEG_1_BG=#3e5a5d
 
-export TMUX_COLOR_SEG_2_FG=colour4
-export TMUX_COLOR_SEG_2_BG=#155352
+export TMUX_COLOR_SEG_2_FG=colour3
+export TMUX_COLOR_SEG_2_BG=#566f71
 
 export TMUX_COLOR_SEG_3_FG=#0f3b3a
-export TMUX_COLOR_SEG_3_BG=#b1c9c3
+export TMUX_COLOR_SEG_3_BG=#b7c1c2
 
 /etc/system/scripts/tmux-statusline.sh
