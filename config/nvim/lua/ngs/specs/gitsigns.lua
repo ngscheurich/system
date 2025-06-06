dcc4ea3cece4557b5dcafb4fb597624446e11fbe
N. G. Scheurich <nick@scheurich.haus>
2025-06-05 19:07:23 -0500

    refactor(nvim): gitsigns config

--- config/nvim/lua/ngs/specs/gitsigns.lua
+++ config/nvim/lua/ngs/specs/gitsigns.lua
@@ -1,8 +1,12 @@
 return {
 	"lewis6991/gitsigns.nvim",
+
+	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", "petertriho/nvim-scrollbar" },
+
 	config = function()
 		local gs = require("gitsigns")
-		local sb_ok, sb_handler = pcall(require, "scrollbar.handlers.search")
+		local sb_handler = require("scrollbar.handlers.search")
+		local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
 
 		gs.setup({
 			signs = {
@@ -20,9 +24,13 @@ return {
 		vim.keymap.set("n", "<Leader>gh", gs.toggle_linehl, { desc = "Line highlight (toggle)" })
 		vim.keymap.set("n", "<Leader>gp", gs.preview_hunk, { desc = "Preview hunk" })
 		vim.keymap.set("n", "<Leader>gr", gs.reset_hunk, { desc = "Reset hunk" })
+		vim.keymap.set("n", "]h", gs.next_hunk, { desc = "Next hunk" })
+		vim.keymap.set("n", "[h", gs.prev_hunk, { desc = "Previous hunk" })
 
-		if sb_ok then
-			sb_handler.setup()
-		end
+		sb_handler.setup()
+
+		local next_hunk_repeat, prev_hunk_repeat = ts_repeat_move.make_repeatable_move_pair(gs.next_hunk, gs.prev_hunk)
+		vim.keymap.set({ "n", "x", "o" }, "]h", next_hunk_repeat)
+		vim.keymap.set({ "n", "x", "o" }, "[h", prev_hunk_repeat)
 	end,
 }
