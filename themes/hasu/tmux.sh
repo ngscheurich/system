37e0a792b094b9b41ef4c78916848f7e2dc9d8fa
N. G. Scheurich <nick@scheurich.haus>
2025-06-25 16:33:43 -0500

    feat(themes): update hasu tmux colors

--- themes/hasu/tmux.sh
+++ themes/hasu/tmux.sh
@@ -12,12 +12,12 @@ export TMUX_COLOR_ACTIVE_FG=colour7
 export TMUX_COLOR_ACTIVE_BG=#fbf7f0
 
 export TMUX_COLOR_SEG_1_FG=colour7
-export TMUX_COLOR_SEG_1_BG=#fbf7f0
+export TMUX_COLOR_SEG_1_BG=#e8e0d0
 
-export TMUX_COLOR_SEG_2_FG=#fbf7f0
-export TMUX_COLOR_SEG_2_BG=colour7
+export TMUX_COLOR_SEG_2_FG=colour7
+export TMUX_COLOR_SEG_2_BG=#dfd6c4
 
-export TMUX_COLOR_SEG_3_FG=#fbf7f0
-export TMUX_COLOR_SEG_3_BG=colour7
+export TMUX_COLOR_SEG_3_FG=colour7
+export TMUX_COLOR_SEG_3_BG=#d4c8b1
 
 /etc/system/scripts/tmux-statusline.sh
