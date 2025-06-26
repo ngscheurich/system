4878a7f11e3235fb21e6a8c9905555bdb6bc8f6f
N. G. Scheurich <nick@scheurich.haus>
2025-06-26 16:39:34 -0500

    chore(nvim): reorganize

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,9 +1,9 @@
 vim.g.ngs = {
-	theme = {
-		name = "default",
-		colorscheme = "default",
-		palette = "default",
-	},
+  theme = {
+    name = "default",
+    colorscheme = "default",
+    palette = "default",
+  },
 }
 
 vim.g.mapleader = " "
@@ -13,17 +13,17 @@ vim.lsp.enable({ "lexical", "lua_ls", "nil", "typescript_ls" })
 
 require("ngs.core.abbrevs")
 require("ngs.core.autocmds")
-require("ngs.core.commands")
-require("ngs.core.diagnostic")
+require("ngs.core.diag")
 require("ngs.core.keymaps")
 require("ngs.core.options")
-require("ngs.core.lazy")
+require("ngs.core.usercmds")
 
 require("ngs.util").load_theme()
 
+require("ngs.lazy")
 require("lazy").setup({
-	spec = { { import = "ngs.specs" } },
-	install = { colorscheme = { vim.g.ngs.theme.colorscheme } },
-	checker = { enabled = true },
-	change_detection = { notify = false },
+  spec = { { import = "ngs.specs" } },
+  install = { colorscheme = { vim.g.ngs.theme.colorscheme } },
+  checker = { enabled = true },
+  change_detection = { notify = false },
 })
