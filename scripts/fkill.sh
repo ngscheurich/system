36b4d44d1e9f6eb1da8e1c0d258d6684c70fe4eb
N. G. Scheurich <nick@scheurich.haus>
2025-06-26 16:41:00 -0500

    feat(scripts): add fkill

---
+++ scripts/fkill.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# See https://github.com/junegunn/fzf/wiki/Examples#processes
+
+set -euo pipefail
+
+if [ "$UID" != "0" ]; then
+  pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+else
+  pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+fi
+
+if [ "x$pid" != "x" ]
+then
+  echo $pid | xargs kill -${1:-9}
+fi
