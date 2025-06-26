fcc404276c76ad14cba670f47ba86b518297d63c
N. G. Scheurich <nick@scheurich.haus>
2025-06-26 14:45:28 -0500

    feat(nvim): update lexical cmd

--- config/nvim/lsp/lexical.lua
+++ config/nvim/lsp/lexical.lua
@@ -1,5 +1,5 @@
 vim.lsp.config["lexical"] = {
-	cmd = { vim.env.HOME .. "/Projects/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
+	cmd = { "lexical" },
 	filetypes = { "elixir", "heex", ".git" },
 	root_markers = { "mix.exs" },
 }
