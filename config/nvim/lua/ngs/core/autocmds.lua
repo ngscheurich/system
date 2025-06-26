71599edef8c77cd662d40626d959dd9f7e820c75
N. G. Scheurich <nick@scheurich.haus>
2025-06-26 14:46:42 -0500

    feat(nvim): set CodeCompanion window bg

--- config/nvim/lua/ngs/core/autocmds.lua
+++ config/nvim/lua/ngs/core/autocmds.lua
@@ -1,9 +1,20 @@
-local ngs_default = vim.api.nvim_create_augroup("ngs.default", {})
+local colors_group = vim.api.nvim_create_augroup("ngs.colors", {})
 
 vim.api.nvim_create_autocmd("ColorScheme", {
 	desc = "Rebuild the status line when the color scheme changes",
-	group = ngs_default,
+	group = colors_group,
 	callback = function()
 		require("ngs.specs.heirline").config()
+	end,
+})
+
+local ft_group = vim.api.nvim_create_augroup("ngs.filetypes", {})
+
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "Change the appearance of special windows",
+	group = ft_group,
+	pattern = "codecompanion",
+	callback = function()
+		vim.opt_local.winhighlight = "Normal:NormalDark"
 	end,
 })
