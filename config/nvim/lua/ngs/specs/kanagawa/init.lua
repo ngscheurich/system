352b61041b45c7a627218f04f45f100d77eecdb6
N. G. Scheurich <nick@scheurich.haus>
2025-06-26 14:55:12 -0500

    feat(nvim): update Kanagawa colors

--- config/nvim/lua/ngs/specs/kanagawa/init.lua
+++ config/nvim/lua/ngs/specs/kanagawa/init.lua
@@ -9,21 +9,22 @@ return {
 			colors = { palette = require("ngs.specs.kanagawa." .. vim.g.ngs.theme.palette) },
 			overrides = function(colors)
 				local theme = colors.theme
-				return {
-					NormalFloat = { bg = "none" },
-					FloatBorder = { bg = "none" },
-					FloatTitle = { bg = "none" },
 
-					-- Save an hlgroup with dark background and dimmed foreground
-					-- so that you can use it where your still want darker windows.
-					-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+				local higroups = {
 					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-					-- Popular plugins that open floats will link to NormalFloat by default;
-					-- set their background accordingly if you wish to keep them dark and borderless
-					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 				}
+
+				local bg_none = { "NormalFloat", "FloatBorder", "FloatTitle" }
+				for _, h in ipairs(bg_none) do
+					higroups[h] = { bg = "none" }
+				end
+
+				local bg_dark = { "LazyNormal", "NeotreeNormal", "NeotreeNormalNC" }
+				for _, h in ipairs(bg_dark) do
+					higroups[h] = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim }
+				end
+
+				return higroups
 			end,
 		})
 
